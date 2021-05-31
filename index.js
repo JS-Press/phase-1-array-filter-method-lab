3ce33f904d3df08681c6cee6a5ce8140cfc09eb3
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-31 17:49:25 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+   
+    
+    const newDrivers = drivers.filter(function(person){return person.toLowerCase() === name.toLowerCase()});
+    return newDrivers;
+}
+
+function fuzzyMatch(drivers, nameBit){
+    const fuzzyDrivers = drivers.filter(function(person){
+        return person.toLowerCase().indexOf(nameBit.toLowerCase()) !== -1
+    });
+    return fuzzyDrivers;
+}
+
+function matchName(drivers,name){
+    const newDrivers = drivers.filter(function(step){drivers[step] === name})
+    return newDrivers;
+}
+
+
+
+
+
+// const fuzzyDrivers = drivers.filter(function(person){
+//     return person.toLowerCase().indexOf(nameBit.toLowerCase()) !== -1
+// });
